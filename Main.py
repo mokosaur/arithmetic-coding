14e60810d5c95def10a75c8d434fb9fdc2a291ad
Tomasz Danel <timages94@gmail.com>
2016-11-17 20:43:03 +0100

    First commit

---
+++ Main.py
@@ -0,0 +1,257 @@
+from fractions import Fraction
+import struct
+
+
+class FileHandler(object):
+    def __init__(self, name):
+        self.__name = name
+
+    def read_file(self):
+        code = ''
+        with open(self.__name, "rb") as f:
+            binary = f.read(4)
+            while binary:
+                i = struct.unpack('i', binary)
+                code += str(bin(i[0]))[2:].zfill(31)
+                binary = f.read(4)
+        return code
+
+    def write_file(self, binary_string):
+        with open(self.__name, "wb") as f:
+            while len(binary_string) > 0:
+                window = binary_string[:31]
+                if len(window) < 31:
+                    window += '0' * (31-len(window))
+                i = int(window, 2)
+                f.write(struct.pack('i', i))
+                binary_string = binary_string[31:]
+
+
+class Coder(object):
+    """Encodes and decodes text using arithmetic coding"""
+    __statistics = {}
+
+    def train_encode(self, text):
+        """
+        Train coder on given text and encode this text.
+
+        Args:
+            text: a piece of text that you want to encode
+        Returns:
+            arithmetic code of given text
+        """
+        self.train(text)
+        return self.encode(text)
+
+    def train(self, text):
+        """
+        Train text statistics from given text string.
+
+        Calculated statistics will be stored in Coder object attributes, and will be used later during encoding or
+        decoding process.
+
+        Args:
+            text: a piece of text from which you want to get the statistics
+        """
+        text += str(0)
+        counts = {}
+        for letter in text:
+            counts[letter] = counts.get(letter, 0) + 1
+
+        self.__statistics = {}
+        start = Fraction(0)
+        for letter in counts.keys():
+            end = start + Fraction(counts[letter], len(text))
+            self.__statistics[letter] = (start, end)
+            start = end
+
+    def encode(self, text):
+        """
+        Encode text using trained statistics.
+
+        Args:
+            text: unicode text you want to compress
+        Returns:
+            arithmetic code of given text
+        """
+        text += str(0)
+        code = ''
+        follow = 0
+        interval = (Fraction(0), Fraction(1))
+        for letter in text:
+            length = interval[1] - interval[0]
+            prob_interval = self.__statistics[letter]
+            interval = (interval[0] + prob_interval[0] * length, interval[0] + prob_interval[1] * length)
+            while True:
+                if interval[1] < Fraction(1, 2):
+                    code += '0' + follow * '1'
+                    interval = (interval[0] * 2, interval[1] * 2)
+                    follow = 0
+                elif interval[0] >= Fraction(1, 2):
+                    code += '1' + follow * '0'
+                    interval = (interval[0] * 2 - 1, interval[1] * 2 - 1)
+                    follow = 0
+                elif interval[0] >= Fraction(1, 4) and interval[1] < Fraction(3, 4):
+                    follow += 1
+                    interval = (interval[0] * 2 - Fraction(1, 2), interval[1] * 2 - Fraction(1, 2))
+                else:
+                    break
+        follow += 1
+        if interval[1] < Fraction(1, 4):
+            code += '0' + follow * '1'
+        else:
+            code += '1' + follow * '0'
+        return code
+
+    def decode(self, code):
+        """
+        Decode binary string using trained statistics.
+
+        Args:
+            code: binary code you want to decode
+        Returns:
+            decoded string
+        """
+        value = Fraction(0)
+        inc = Fraction(1)
+        for i in range(len(code)):
+            inc /= 2
+            value += int(code[i]) * inc
+
+        text = ''
+        interval = (Fraction(0), Fraction(1))
+        while True:
+            length = interval[1] - interval[0]
+            letter, prob_interval = self.get_letter((value - interval[0]) * 1 / length)
+            text += letter
+            if letter == str(0):
+                break
+            interval = (interval[0] + prob_interval[0] * length, interval[0] + prob_interval[1] * length)
+            while True:
+                if interval[1] < Fraction(1, 2):
+                    interval = (interval[0] * 2, interval[1] * 2)
+                    value *= 2
+                elif interval[0] >= Fraction(1, 2):
+                    interval = (interval[0] * 2 - 1, interval[1] * 2 - 1)
+                    value = value * 2 - 1
+                elif interval[0] >= Fraction(1, 4) and interval[1] < Fraction(3, 4):
+                    value = value * 2 - Fraction(1, 2)
+                    interval = (interval[0] * 2 - Fraction(1, 2), interval[1] * 2 - Fraction(1, 2))
+                else:
+                    break
+        return text[0:-1]
+
+    def get_letter(self, value):
+        """
+        Get letter and cumulative probability from trained statistics.
+
+        Args:
+            value: value in range [0,1) you want to find in statistics
+        Returns:
+            tuple (a,b) where:
+            a: letter
+            b: probability interval (tuple)
+        """
+        for key, i in self.__statistics.items():
+            if value >= i[0] and value < i[1]:
+                return key, i
+        raise 'Value not found'
+
+
+def main():
+    coder = Coder()
+    fh = FileHandler("oda.txt")
+    text = """Bez serc, bez ducha, to szkieletów ludy;
+Młodości! dodaj mi skrzydła!
+Niech nad martwym wzlecę światem
+W rajską dziedzinę ułudy:
+Kędy zapał tworzy cudy,
+Nowości potrząsa kwiatem
+I obleka w nadziei złote malowidła.
+
+Niechaj, kogo wiek zamroczy,
+Chyląc ku ziemi poradlone czoło,
+Takie widzi świata koło,
+Jakie tępymi zakreśla oczy.
+Młodości! ty nad poziomy
+Wylatuj, a okiem słońca
+Ludzkości całe ogromy
+Przeniknij z końca do końca.
+
+Patrz na dół - kędy wieczna mgła zaciemia
+Obszar gnuśności zalany odmętem;
+To ziemia!
+Patrz. jak nad jej wody trupie
+Wzbił się jakiś płaz w skorupie.
+Sam sobie sterem, żeglarzem, okrętem;
+Goniąc za żywiołkami drobniejszego płazu,
+To się wzbija, to w głąb wali;
+Nie lgnie do niego fala, ani on do fali;
+A wtem jak bańka prysnął o szmat głazu.
+Nikt nie znał jego życia, nie zna jego zguby:
+To samoluby!
+
+Młodości! tobie nektar żywota
+Natenczas słodki, gdy z innymi dzielę:
+Serca niebieskie poi wesele,
+Kiedy je razem nić powiąże złota.
+Razem, młodzi przyjaciele!...
+W szczęściu wszystkiego są wszystkich cele;
+Jednością silni, rozumni szałem,
+Razem, młodzi przyjaciele!...
+I ten szczęśliwy, kto padł wśród zawodu,
+Jeżeli poległym ciałem
+Dał innym szczebel do sławy grodu.
+Razem, młodzi przyjaciele!...
+Choć droga stroma i śliska,
+Gwałt i słabość bronią wchodu:
+Gwałt niech się gwałtem odciska,
+A ze słabością łamać uczmy się za młodu!
+
+Dzieckiem w kolebce kto łeb urwał Hydrze,
+Ten młody zdusi Centaury,
+Piekłu ofiarę wydrze,
+Do nieba pójdzie po laury.
+Tam sięgaj, gdzie wzrok nie sięga;
+Łam, czego rozum nie złamie:
+Młodości! orla twych lotów potęga,
+Jako piorun twoje ramię.
+
+Hej! ramię do ramienia! spólnymi łańcuchy
+Opaszmy ziemskie kolisko!
+Zestrzelmy myśli w jedno ognisko
+I w jedno ognisko duchy!...
+Dalej, bryło, z posad świata!
+Nowymi cię pchniemy tory,
+Aż opleśniałej zbywszy się kory,
+Zielone przypomnisz lata.
+
+A jako w krajach zamętu i nocy,
+Skłóconych żywiołów waśnią,
+Jednym "stań się" z bożej mocy
+Świat rzeczy stanął na zrębie;
+Szumią wichry, cieką głębie,
+A gwiazdy błękit rozjaśnią -
+
+W krajach ludzkości jeszcze noc głucha:
+Żywioły chęci jeszcze są w wojnie;
+Oto miłość ogniem zionie,
+Wyjdzie z zamętu świat ducha:
+Młodość go pocznie na swoim łonie,
+A przyjaźń w wieczne skojarzy spojnie.
+
+Pryskają nieczułe lody
+I przesądy światło ćmiące;
+Witaj, jutrzenko swobody,
+Zbawienia za tobą słońce!"""
+    code = coder.train_encode(text)
+    print(code)
+    fh.write_file(code)
+    with open("oda2.txt", "w") as f:
+        f.write(text)
+    print(fh.read_file())
+    print(coder.decode(fh.read_file()))
+
+
+if __name__ == "__main__":
+    main()
